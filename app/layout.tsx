af1cd12217b87b89fdb6da4912799938fc85b7b0
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-05-27 01:13:30 -0700

    Add Twitter OG Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
+    title: siteConfig.name,
     description: siteConfig.description,
     images: [
       {
