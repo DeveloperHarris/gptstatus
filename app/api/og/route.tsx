babe6f957d6085b903999d99f523beb0b9a87cf8
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-05-27 00:55:08 -0700

    Fix config on og route

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse } from "@vercel/og"
 
-export const config = {
-  runtime: "edge",
-}
+export const runtime = "edge"
 
 export async function GET(request: Request) {
   return new ImageResponse(
